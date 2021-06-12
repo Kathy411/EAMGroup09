c57786f42060a3b54358f1ace280ec4264ade5ac
kathy <katharina.linden@outlook.com>
2021-06-12 12:00:35 +0200

    Changes in layouts

--- app/src/main/java/com/softbankrobotics/eamgroup09/AnimationFunctions.kt
+++ app/src/main/java/com/softbankrobotics/eamgroup09/AnimationFunctions.kt
@@ -126,3 +126,27 @@ fun basketball(qiContext: QiContext) {
             .build()                                            // Build the animate action.
     basketballAnimate.run()
 }
+
+fun hello(qiContext: QiContext) {
+    val helloAnim: Animation = AnimationBuilder.with(qiContext) // Create the builder with the context.
+            .withResources(R.raw.anim_hello)                    // Set the animation resource.
+            .build()                                            // Build the animation.
+
+    // Create an animate action.
+    val helloAnimate: Animate = AnimateBuilder.with(qiContext)  // Create the builder with the context.
+            .withAnimation(helloAnim)                           // Set the animation.
+            .build()                                            // Build the animate action.
+    helloAnimate.run()
+}
+
+fun clapping(qiContext: QiContext) {
+    val clappingAnim: Animation = AnimationBuilder.with(qiContext) // Create the builder with the context.
+            .withResources(R.raw.anim_clap)                    // Set the animation resource.
+            .build()                                            // Build the animation.
+
+    // Create an animate action.
+    val clappingAnimate: Animate = AnimateBuilder.with(qiContext)  // Create the builder with the context.
+            .withAnimation(clappingAnim)                           // Set the animation.
+            .build()                                            // Build the animate action.
+    clappingAnimate.run()
+}
