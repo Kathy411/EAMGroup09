8a9b749ce493ff905825498e164809fe6838d191
kathy <katharina.linden@outlook.com>
2021-06-16 15:33:53 +0200

    Slight changes

--- app/src/main/java/com/softbankrobotics/eamgroup09/SmsActivity.kt
+++ app/src/main/java/com/softbankrobotics/eamgroup09/SmsActivity.kt
@@ -1,18 +1,22 @@
 package com.softbankrobotics.eamgroup09
 
 import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.telephony.SmsManager
 import android.text.TextUtils
 import android.view.View
+import android.view.View.OnFocusChangeListener
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.aldebaran.qi.sdk.QiContext
@@ -24,14 +28,21 @@ import com.aldebaran.qi.sdk.`object`.locale.Locale
 import com.aldebaran.qi.sdk.`object`.locale.Region
 import com.aldebaran.qi.sdk.builder.SayBuilder
 import com.aldebaran.qi.sdk.design.activity.RobotActivity
+import kotlin.concurrent.thread
 
 
+@Suppress("DEPRECATION")
 class SmsActivity : RobotActivity(), RobotLifecycleCallbacks {
-    lateinit var sendButton: Button
+    lateinit var btn_record : Button
+    lateinit var btn_send: Button
     lateinit var editTextNumber: EditText
     lateinit var editTextMessage: EditText
     lateinit var locale : Locale
     private val permissionRequest = 101
+
+    companion object {
+        private const val REQUEST_CODE_STT = 1
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,11 +50,12 @@ class SmsActivity : RobotActivity(), RobotLifecycleCallbacks {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sms)
         editTextNumber = findViewById(R.id.et_number)
-        showSoftKeyboard(editTextNumber)
+        editTextNumber.onFocusChangeListener = OnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                showSoftKeyboard(editTextNumber)
+            }
+        }
         editTextMessage = findViewById(R.id.et_message)
-        showSoftKeyboard(editTextMessage)
-        sendButton = findViewById(R.id.btn_send)
-
     }
 
     override fun onRobotFocusGained(qiContext: QiContext?) {
@@ -54,16 +66,37 @@ class SmsActivity : RobotActivity(), RobotLifecycleCallbacks {
         }
 
         locale = Locale(Language.GERMAN, Region.GERMANY)
-        val smsPhrase : Phrase = Phrase("Von hier aus kannst Du SMS versenden. Derzeit musst Du Text und Nummer noch eingeben, " +
+        val smsPhrase: Phrase = Phrase("Von hier aus kannst Du SMS versenden. Derzeit musst Du Text und Nummer noch eingeben, " +
                 "aber meine Programmierer Innen arbeiten an der Spracheingabe. Wenn Du fertig bist, drücke einfach auf Senden")
         val smsSay = SayBuilder.with(qiContext).withPhrase(smsPhrase).withLocale(locale).build()
-
         smsSay.async().run()
 
-        sendButton.setOnClickListener {
+         btn_send = findViewById(R.id.btn_send)
+         btn_send.setOnClickListener {
             sendMessage()
-            //   }
         }
+
+    /*    btn_record = findViewById(R.id.btn_talk)
+        btn_record.setOnClickListener {
+            // Get the Intent action
+            val sttIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            // Language model defines the purpose, there are special models for other use cases, like search.
+            sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            // Adding an extra language, you can use any language from the Locale class.
+            sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, java.util.Locale.GERMAN)
+            // Text that shows up on the Speech input prompt.
+            sttIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Du kannst Deine Nachricht jetzt aufsprechen")
+            try {
+                // Start the intent for a result, and pass in our request code.
+                startActivityForResult(sttIntent, REQUEST_CODE_STT)
+            } catch (e: ActivityNotFoundException) {
+                // Handling error when the service is not available.
+                e.printStackTrace()
+                Toast.makeText(this, "Dein Gerät unterstützt leider keine Texteingabe", Toast.LENGTH_LONG).show()
+            }
+        }
+
+     */
     }
 
     private fun showSoftKeyboard(view: View) {
@@ -106,13 +139,39 @@ class SmsActivity : RobotActivity(), RobotLifecycleCallbacks {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == permissionRequest) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                myMessage();
+                myMessage()
             } else {
                 Toast.makeText(this, "Nachricht konnte leider nicht versendet werden!",
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+    // STT
+   /* override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        editTextMessage = findViewById(R.id.et_message)
+        when (requestCode) {
+            // Handle the result for our request code.
+            REQUEST_CODE_STT -> {
+                // Safety checks to ensure data is available.
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    // Retrieve the result array.
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    // Ensure result array is not null or empty to avoid errors.
+                    if (!result.isNullOrEmpty()) {
+                        // Recognized text is in the first position.
+                        val recognizedText = result[0]
+                        // Do what you want with the recognized text.
+                        editTextMessage.setText(recognizedText)
+                    }
+                }
+            }
+        }
+    }
+    
+    */
+
     override fun onRobotFocusLost() {
         TODO("Not yet implemented")
     }
@@ -120,5 +179,4 @@ class SmsActivity : RobotActivity(), RobotLifecycleCallbacks {
     override fun onRobotFocusRefused(reason: String?) {
         TODO("Not yet implemented")
     }
-
 }
